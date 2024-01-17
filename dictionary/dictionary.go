312fa62bca1de67e8f9dd6a42336eea6f5388abd
Mktm <mody.kane60100@gmail.com>
2024-01-17 14:58:53 +0100

    add file register

--- dictionary/dictionary.go
+++ dictionary/dictionary.go
@@ -1,11 +1,6 @@
 package dictionary
 
-import (
-	"bufio"
-	"fmt"
-	"strconv"
-	"strings"
-)
+import "fmt"
 
 type Entry struct {
 	Word       string
@@ -17,105 +12,37 @@ func (e Entry) String() string {
 }
 
 type Dictionary struct {
-	entries map[string]Entry
-}
-
-func GetUserChoice(reader *bufio.Reader) (int, error) {
-	fmt.Print("Enter your choice: ")
-	text, err := reader.ReadString('\n')
-	if err != nil {
-		return 0, err
-	}
-	text = strings.TrimSpace(text)
-	choice, err := strconv.Atoi(text)
-	if err != nil {
-		return 0, err
-	}
-	return choice, nil
+   entries map[string]Entry
 }
 
 func New() *Dictionary {
-	return &Dictionary{
-		entries: make(map[string]Entry),
-	}
+   return &Dictionary{
+      entries: make(map[string]Entry),
+   }
 }
 
 func (d *Dictionary) Add(word string, definition string) {
-	entry := Entry{Word: word, Definition: definition}
-	d.entries[word] = entry
-}
-
-func ActionAdd(dic *Dictionary, reader *bufio.Reader) {
-	fmt.Print("Enter the word: ")
-	word, _ := reader.ReadString('\n')
-	word = strings.TrimSpace(word)
-
-	fmt.Print("Enter the definition: ")
-	definition, _ := reader.ReadString('\n')
-	definition = strings.TrimSpace(definition)
-
-	dic.Add(word, definition)
-	fmt.Printf("Word '%s' added with definition '%s'.\n", word, definition)
+   entry := Entry{Word: word, Definition: definition}
+   d.entries[word] = entry
 }
 
 func (d *Dictionary) Get(word string) (Entry, error) {
-	entry, found := d.entries[word]
-	if !found {
-		return Entry{}, fmt.Errorf("word '%s' not found", word)
-	}
-	return entry, nil
-}
-
-func ActionDefine(dic *Dictionary, reader *bufio.Reader) {
-	fmt.Print("Enter the word : ")
-	word, _ := reader.ReadString('\n')
-	word = strings.TrimSpace(word)
-
-	entry, err := dic.Get(word)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	fmt.Print("Enter the new definition : ")
-	newDefinition, _ := reader.ReadString('\n')
-	newDefinition = strings.TrimSpace(newDefinition)
-	dic.Remove(word)
-	dic.Add(word, newDefinition)
-	fmt.Printf("Definition of '%s' : '%s\n'", entry.Word, entry.Definition)
-}
-
-func (d *Dictionary) Remove(word string) {
-	delete(d.entries, word)
-}
-
-func ActionRemove(dic *Dictionary, reader *bufio.Reader) {
-	fmt.Print("Enter the word to remove: ")
-	word, _ := reader.ReadString('\n')
-	word = strings.TrimSpace(word)
-
-	entry, err := dic.Get(word)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	dic.Remove(word)
-	fmt.Printf("Word '%s' removed.\n", entry.Word)
+   entry, found := d.entries[word]
+   if !found {
+      return Entry{}, fmt.Errorf("word '%s' not found", word)
+   }
+   return entry, nil
 }
 
 func (d *Dictionary) List() ([]string, map[string]Entry) {
 	wordList := make([]string, 0, len(d.entries))
 	for word := range d.entries {
-		wordList = append(wordList, word)
+	   wordList = append(wordList, word)
 	}
 	return wordList, d.entries
+ }
+ 
+func (d *Dictionary) Remove(word string) {
+   delete(d.entries, word)
 }
 
-func ActionList(dic *Dictionary) {
-	words, entries := dic.List()
-	fmt.Println("Words in the dictionary:")
-	for _, word := range words {
-		fmt.Printf("%s: %s\n", word, entries[word])
-	}
-}
--- main.go
+++ main.go
@@ -2,14 +2,27 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 
 	"manipulation/dictionary"
 )
 
+const dictionaryFilePath = "dictionary.json"
+
+
+type SaveData struct {
+	Entries map[string]struct {
+		Definition string `json:"definition"`
+	} `json:"entries"`
+}
+
 func main() {
-	dic := dictionary.New()
+	d := loadDictionary()
+
 	reader := bufio.NewReader(os.Stdin)
 
 	for {
@@ -20,7 +33,7 @@ func main() {
 		fmt.Println("4. List")
 		fmt.Println("5. Exit")
 
-		choice, err := dictionary.GetUserChoice(reader)
+		choice, err := getUserChoice(reader)
 		if err != nil {
 			fmt.Println("Choice Error:", err)
 			continue
@@ -28,18 +41,143 @@ func main() {
 
 		switch choice {
 		case 1:
-			dictionary.ActionAdd(dic, reader)
+			actionAdd(d, reader)
 		case 2:
-			dictionary.ActionDefine(dic, reader)
+			actionDefine(d, reader)
 		case 3:
-			dictionary.ActionRemove(dic, reader)
+			actionRemove(d, reader)
 		case 4:
-			dictionary.ActionList(dic)
+			actionList(d)
 		case 5:
+			saveDictionary(d)
 			fmt.Println("Exit program.")
 			return
 		default:
 			fmt.Println("Invalid choice. Please choose an option valid.")
 		}
 	}
+}
+
+func getUserChoice(reader *bufio.Reader) (int, error) {
+	fmt.Print("Enter your choice: ")
+	text, err := reader.ReadString('\n')
+	if err != nil {
+		return 0, err
+	}
+	text = strings.TrimSpace(text)
+	choice, err := strconv.Atoi(text)
+	if err != nil {
+		return 0, err
+	}
+	return choice, nil
+}
+
+func actionAdd(d *dictionary.Dictionary, reader *bufio.Reader) {
+	fmt.Print("Enter the word: ")
+	word, _ := reader.ReadString('\n')
+	word = strings.TrimSpace(word)
+
+	fmt.Print("Enter the definition: ")
+	definition, _ := reader.ReadString('\n')
+	definition = strings.TrimSpace(definition)
+
+	d.Add(word, definition)
+	fmt.Printf("Word '%s' added with definition '%s'.\n", word, definition)
+}
+
+func actionDefine(d *dictionary.Dictionary, reader *bufio.Reader) {
+	fmt.Print("Enter the word : ")
+	word, _ := reader.ReadString('\n')
+	word = strings.TrimSpace(word)
+
+	entry, err := d.Get(word)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Print("Enter the new definition : ")
+	newDefinition, _ := reader.ReadString('\n')
+	newDefinition = strings.TrimSpace(newDefinition)
+	d.Remove(word)
+	d.Add(word, newDefinition)
+	fmt.Printf("Definition of '%s' : '%s\n'", entry.Word, entry.Definition)
+}
+
+func actionList(d *dictionary.Dictionary) {
+	words, entries := d.List()
+	fmt.Println("Words in the dictionary:")
+	for _, word := range words {
+		fmt.Printf("%s: %s\n", word, entries[word])
+	}
+}
+
+func actionRemove(d *dictionary.Dictionary, reader *bufio.Reader) {
+	fmt.Print("Enter the word to remove: ")
+	word, _ := reader.ReadString('\n')
+	word = strings.TrimSpace(word)
+
+	entry, err := d.Get(word)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	d.Remove(word)
+	fmt.Printf("Word '%s' removed.\n", entry.Word)
+}
+
+
+func saveDictionary(d *dictionary.Dictionary) {
+	saveData := SaveData{
+		Entries: make(map[string]struct {
+			Definition string `json:"definition"`
+		}),
+	}
+
+	words, entries := d.List()
+	for _, word := range words {
+		saveData.Entries[word] = struct {
+			Definition string `json:"definition"`
+		}{Definition: entries[word].Definition}
+	}
+
+	data, err := json.MarshalIndent(saveData, "", "  ")
+	if err != nil {
+		fmt.Println("Error encoding dictionary:", err)
+		return
+	}
+
+	err = os.WriteFile(dictionaryFilePath, data, 0644)
+	if err != nil {
+		fmt.Println("Error saving dictionary to file:", err)
+	}
+}
+
+func loadDictionary() *dictionary.Dictionary {
+	d := dictionary.New()
+
+	data, err := os.ReadFile(dictionaryFilePath)
+	if err != nil {
+		fmt.Println("Error reading dictionary file:", err)
+		return d
+	}
+
+	
+	if len(data) == 0 {
+		return d
+	}
+
+	var saveData SaveData
+	err = json.Unmarshal(data, &saveData)
+	if err != nil {
+		fmt.Println("Error decoding dictionary:", err)
+		return d
+	}
+
+	for word, entry := range saveData.Entries {
+		d.Add(word, entry.Definition)
+	}
+
+	return d
 }
